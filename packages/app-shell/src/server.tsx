f600a16c76271390d8cbdcbf86c3cbcf38795a29
Kasper Moskwiak <kasper.moskwiak@gmail.com>
2024-06-13 23:23:33 +0200

    pagespeed fixes

--- packages/app-shell/src/server.tsx
+++ packages/app-shell/src/server.tsx
@@ -38,6 +38,14 @@ app.use("/static", express.static(path.resolve(__dirname, "../../dist")));
 
 app.use(morgan("combined"));
 
+app.use("/fonts.css", (_req, res) => {
+  res.setHeader("Content-Type", "text/css");
+  res.send(`
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+  `);
+});
+
 app.use("/", async (req, res, next) => {
 
   if (!["/", "/articles", "/projects", "/loading"].includes(req.path)) {
